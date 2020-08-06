566157890244f3b97c60e56a3b7ac96c69d34311
Aijanss <68231963+Aijanss@users.noreply.github.com>
2020-08-06 13:41:04 +0600

    else

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 40;
+        if(num>40){
+            System.out.println("Вы сохранили положительное число");
+                }
+        if(num==40){
+            System.out.println("Вы сохранили сохранили ноль ");
+        }
+                if(num<40){
+                    System.out.println("Вы сохранили отрецательное число ");
+                }else{
+                    System.out.println("Вы  не  можите сохранили это число");
+            }
+        }
+    }
+
