f5497056adb92f7ce35c5720e0aa0ff4c9e42d4d
Aijanss <68231963+Aijanss@users.noreply.github.com>
2020-08-12 10:26:56 +0600

    Дз

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,39 @@
 package com.company;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String republicInCentralAsia ="Независимоя страна"+ " "+" " + "Кыргызстан"+  ".";
+        String republicInCentralAsia = "Независимоя страна" + " " + " " + "Кыргызстан" + ".";
         System.out.println(republicInCentralAsia);
-        String word = "Одина из красивейших стран в центральной Азии "+ " ."+ "Получивший не зависимость в августе";
+        String word = "Одна из красивейших стран в центральной Азии " + " ." + "Получивший не зависимость в августе";
         final int num = 1991;
         {
-            System.out.println(republicInCentralAsia + " "+ " " +word + " " + num);
+            System.out.println(republicInCentralAsia + " " + " " + word + " " + num);
         }
-        int num1 = 1991;
+        final int num1 = 1991;
         {
 
         }
-        if (num1 > 1000) {
-            System.out.println("Вы сохранили положительное число");
-        }
-        if (num1 == 1000) {
-            System.out.println("Вы сохранили сохранили ноль ");
+        if (num1 >1000) {
+            System.out.println("Вы сохранили  ноль ");
         }
         if (num1 < 1000) {
             System.out.println("Вы сохранили отрецательное число ");
         } else {
             System.out.println("Вы  не  можите сохранили это число");
         }
+    Scanner in = new Scanner(System.in);
+        System.out.println("Input a name: Добрый день");
+        String name=in.nextLine();
+        System.out.println("Добрый день:name");
+
     }
 }
+
+
+
+
 
